50e43ce256a8b93b435c98f766db0c1bd171cf6e
tompall <pall.tamas1@gmail.com>
2018-03-01 19:14:30 +0100

    CPP TESZT
    CPP TESZT

---
+++ Source/LEGENDA_GIT_TEST/Pocak.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Pocak.h"
+
+
+// Sets default values
+APocak::APocak()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APocak::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APocak::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
