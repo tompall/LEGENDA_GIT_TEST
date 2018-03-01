ae7191d796239006a093ec6a9fd1e4800353dc38
tompall <pall.tamas1@gmail.com>
2018-03-01 18:07:03 +0100

    Initial commit

---
+++ Source/LEGENDA_GIT_TEST/LEGENDA_GIT_TEST.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LEGENDA_GIT_TEST.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LEGENDA_GIT_TEST, "LEGENDA_GIT_TEST" );
---
+++ Source/LEGENDA_GIT_TEST/LEGENDA_GIT_TESTGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LEGENDA_GIT_TESTGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEGENDA_GIT_TEST_API ALEGENDA_GIT_TESTGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
